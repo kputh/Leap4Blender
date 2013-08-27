4bcc2c55cdf566bc462d246dff042b482708d528
Kai Puth <kputh@users.noreply.github.com>
2013-08-27 13:58:57 +0000

    Position und Orientierung des Kegels stimmen. Die Orientierung ist nicht sehr stabil. Der Kegel überdeckt den Finger, sollte aber eigentlich aus der Fingerspitze hinausprojeziert werden.

--- PointToSelect.py
+++ PointToSelect.py
@@ -56,13 +56,20 @@ class PointToSelectOperator(bpy.types.Operator):
             leapPointables = {pointable.id: pointable for pointable in frame.pointables}
             leapPointableIDs = leapPointables.keys()
             blenderPointableIDs = self.tracedPointables.keys()
+
+            # filter by gesture
+            tmp = dict(leapPointables)
+            for id in tmp.keys():
+                if len(tmp[id].hand.pointables) != 1:
+                    del leapPointables[id]
     
             # add new pointable
             for id in leapPointableIDs - blenderPointableIDs:
                 # create model
-                bpy.ops.mesh.primitive_cone_add(vertices=32, radius1=1.0, radius2=1.0, depth=100.0, end_fill_type='NGON',
+                depth = 10.0
+                bpy.ops.mesh.primitive_cone_add(vertices=32, radius1=1.0, radius2=0.0, depth=depth, end_fill_type='NGON',
                 view_align=False, enter_editmode=False,
-                location=(0.0, 0.0, 0.0),
+                location=(0.0, 0.0, 0.0),#location=(0.0, 0.0, -depth / 2.0),
                 rotation=(0.0, 0.0, 0.0))
                 ob = bpy.context.object
                 ob.name = "pointable{}".format(id)
@@ -80,24 +87,34 @@ class PointToSelectOperator(bpy.types.Operator):
             # update visible pointables
             v3d = context.space_data
             rv3d = v3d.region_3d
+            offset = mathutils.Vector((0.0, -300.0, 150.0))    # offset in mm between Leap Motion coordinate system and user coordinate system
             for id in self.tracedPointables:
                 pointable = leapPointables[id]
                 ob = self.tracedPointables[id]
-                # update position
+                
+                # update location
                 tip_position = mathutils.Vector((pointable.tip_position.x, pointable.tip_position.y, pointable.tip_position.z))
+                '''
+                point2 = mathutils.Vector((pointable.tip_position.x, pointable.tip_position.y, pointable.tip_position.z))
+                endpoint1 = mathutils.geometry.intersect_line_plane(tip_position, line_b, plane_co, plane_no, no_flip=False)
+                '''
+                tip_position += offset
                 tip_position *= PointToSelectOperator.SCALE
                 tip_position.rotate(rv3d.view_rotation)
                 ob.location = rv3d.view_location + tip_position
-                
+                # Fehler: Mittelpunkt des Objekts wird auf die Position der Fingerspitze gelegt.
+        
                 # update direction
-                #directionEuler = mathutils.Euler((-leap_finger.direction.pitch, leap_finger.direction.yaw, -leap_finger.direction.roll), 'XYZ')
-                #directionEuler = mathutils.Euler((pointable.direction.pitch, -pointable.direction.yaw, pointable.direction.roll), 'XYZ')
-                #directionEuler = mathutils.Euler((pointable.direction.pitch, 0.0, 0.0), 'XYZ')
-                directionEuler = mathutils.Euler((0.0, -pointable.direction.yaw, 0.0), 'XYZ')
+                #directionEuler = mathutils.Euler((pointable.direction.pitch, -pointable.direction.yaw, pointable.hand.palm_normal.roll), 'XYZ')
+                directionEuler = mathutils.Euler((pointable.direction.pitch, -pointable.direction.yaw, -pointable.hand.palm_normal.roll), 'XYZ')
+                #directionEuler = mathutils.Euler((pointable.direction.pitch, -pointable.direction.yaw, 0.0), 'XYZ')
+                #directionEuler = mathutils.Euler((pointable.direction.pitch, 0.0, 0.0), 'XYZ')  # ok
+                #directionEuler = mathutils.Euler((0.0, -pointable.direction.yaw, 0.0), 'XYZ')   # ok
+                #directionEuler = mathutils.Euler((0.0, 0.0, pointable.direction.roll), 'XYZ')   # ?
                 directionQuaternion = directionEuler.to_quaternion()
                 ob.rotation_mode ='QUATERNION'
                 ob.rotation_quaternion = rv3d.view_rotation * directionQuaternion
-        
+                
         return {'PASS_THROUGH'}
 
     def execute(self, context):
